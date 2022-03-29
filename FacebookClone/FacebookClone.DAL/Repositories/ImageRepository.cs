ff716aed7e0b8440c4d53e2a9098e899002d6285
Danijel Lovas <danijellovas@gmail.com>
2022-03-29 10:30:02 +0200

    Add image repository

---
+++ FacebookClone/FacebookClone.DAL/Repositories/ImageRepository.cs
@@ -0,0 +1,23 @@
+﻿using FacebookClone.DAL.Entities;
+using FacebookClone.DAL.Entities.Context;
+using FacebookClone.DAL.Repositories.Abstract;
+using FacebookClone.DAL.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookClone.DAL.Repositories
+{
+    public class ImageRepository : EfCoreRepository<Image, int, FacebookCloneDBContext>, IImageRepository
+    {
+        public ImageRepository(FacebookCloneDBContext context) : base(context)
+        {
+        }
+        public IEnumerable<Image> GetAllByAlbumId(int albumId)
+        {
+            return GetAll().Where(i => i.AlbumId.Equals(albumId));
+        }
+    }
+}
