a31564e483bfa4e2cbcdfc7022cc5a1a5c2cc7dc
Danijel Lovas <danijellovas@gmail.com>
2022-03-29 11:59:45 +0200

    Add two factor authenticator repository

---
+++ FacebookClone/FacebookClone.DAL/Repositories/TwoFactorAuthenticatorRepository.cs
@@ -0,0 +1,25 @@
+﻿using FacebookClone.DAL.Entities;
+using FacebookClone.DAL.Entities.Context;
+using FacebookClone.DAL.Repositories.Abstract;
+using FacebookClone.DAL.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookClone.DAL.Repositories
+{
+    public class TwoFactorAuthenticatorRepository : EfCoreRepository<TwoFactorAuthentication, FacebookCloneDBContext>
+        , ITwoFactorAuthenticatorRepository
+    {
+        public TwoFactorAuthenticatorRepository(FacebookCloneDBContext context) : base(context)
+        {
+        }
+
+        public TwoFactorAuthentication GetTwoFactorAuthenticationByUserId(int userId)
+        {
+            return GetAll().Find(t => t.UserId == userId);
+        }
+    }
+}
