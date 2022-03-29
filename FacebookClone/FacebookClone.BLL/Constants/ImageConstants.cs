ddda7790e069eec7dad4440624a248a7db7f2a55
Danijel Lovas <danijellovas@gmail.com>
2022-03-29 15:44:56 +0200

    Add constants for image folders

--- FacebookClone/FacebookClone.BLL/Constants/ImageConstants.cs
+++ FacebookClone/FacebookClone.BLL/Constants/ImageConstants.cs
@@ -13,5 +13,11 @@ namespace FacebookClone.BLL.Constants
         public static readonly int MaxImageSize = 62914560;
 
         public static readonly IEnumerable<string> AllowedImageExtensions = new List<string>() { ".png", ".jpg", ".jpeg" };
+
+        public static readonly string UserProfileImageFolder = "User/Profile";
+
+        public static readonly string UserCoverImageFolder = "User/Cover";
+
+        public static readonly string ImageFolder = "Images";
     }
 }
