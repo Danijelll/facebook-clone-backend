a1ed682e2ca9e5e0e2c9e319df07f0b1aad5559d
Danijel Lovas <danijellovas@gmail.com>
2022-03-29 13:46:09 +0200

    Add user mapper

---
+++ FacebookClone/FacebookClone.BLL/Mappers/UserMapper.cs
@@ -0,0 +1,40 @@
+﻿using FacebookClone.BLL.DTO;
+using FacebookClone.DAL.Entities;
+
+namespace FacebookClone.BLL.Mappers
+{
+    public static class UserMapper
+    {
+        public static UserDTO toDTO(this User user)
+        {
+            return new UserDTO()
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Email = user.Email,
+                Password = user.Password,
+                IsEmailConfirmed = user.IsEmailConfirmed,
+                ProfileImage = user.ProfileImage,
+                CoverImage = user.CoverImage,
+                CreatedOn = user.CreatedOn,
+                UpdatedOn = user.UpdatedOn,
+            };
+        }
+
+        public static User ToEntity(this UserDTO user)
+        {
+            return new User()
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Email = user.Email,
+                Password = user.Password,
+                IsEmailConfirmed = user.IsEmailConfirmed,
+                ProfileImage = user.ProfileImage,
+                CoverImage = user.CoverImage,
+                CreatedOn = user.CreatedOn,
+                UpdatedOn = user.UpdatedOn,
+            };
+        }
+    }
+}
