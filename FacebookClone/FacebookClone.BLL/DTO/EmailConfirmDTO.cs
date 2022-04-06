89410f7085e71a9b74de627f92133aa8b148576b
Danijel Lovas <danijellovas@gmail.com>
2022-04-06 11:00:41 +0200

    Add email confirm dto and mapper

---
+++ FacebookClone/FacebookClone.BLL/DTO/EmailConfirmDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FacebookClone.BLL.DTO
+{
+    public class EmailConfirmDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string EmailHash { get; set; } = null!;
+    }
+}
---
+++ FacebookClone/FacebookClone.BLL/Mappers/EmailConfirmMapper.cs
@@ -0,0 +1,33 @@
+﻿using FacebookClone.BLL.DTO;
+using FacebookClone.DAL.Entities;
+
+namespace FacebookClone.BLL.Mappers
+{
+    public static class EmailConfirmMapper
+    {
+        public static EmailConfirmDTO ToDTO(this EmailConfirm emailConfirm)
+        {
+            return new EmailConfirmDTO()
+            {
+                Id = emailConfirm.Id,
+                UserId = emailConfirm.UserId,
+                EmailHash = emailConfirm.EmailHash,
+            };
+        }
+
+        public static EmailConfirm ToEntity(this EmailConfirmDTO emailConfirm)
+        {
+            return new EmailConfirm()
+            {
+                Id = emailConfirm.Id,
+                UserId = emailConfirm.UserId,
+                EmailHash = emailConfirm.EmailHash,
+            };
+        }
+
+        public static IEnumerable<EmailConfirmDTO> ToDTOList(this IEnumerable<EmailConfirm> emailConfirm)
+        {
+            return emailConfirm.Select(x => x.ToDTO()).ToList();
+        }
+    }
+}
