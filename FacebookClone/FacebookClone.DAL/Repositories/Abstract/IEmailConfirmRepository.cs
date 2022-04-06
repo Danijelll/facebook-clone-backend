228e86bf7a32babf4264cd4c1f916e3afb98e98d
Danijel Lovas <danijellovas@gmail.com>
2022-04-06 10:51:17 +0200

    Add email confirm repositories

---
+++ FacebookClone/FacebookClone.DAL/Repositories/Abstract/IEmailConfirmRepository.cs
@@ -0,0 +1,11 @@
+﻿using FacebookClone.DAL.Entities;
+
+namespace FacebookClone.DAL.Repositories.Abstract
+{
+    public interface IEmailConfirmRepository : IRepository<EmailConfirm>
+    {
+        public EmailConfirm GetByUserId(int userId);
+
+        public EmailConfirm GetByEmailHash(string emailHash);
+    }
+}
