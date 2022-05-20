96aea55b42b6aff60b98829cb55e1d58e0be58c6
Danijel Lovas <danijellovas@gmail.com>
2022-05-20 10:50:58 +0200

    Add working one to one user chat

--- FacebookClone/FacebookClone.Presentation/Hubs/ChatHub.cs
+++ FacebookClone/FacebookClone.Presentation/Hubs/ChatHub.cs
@@ -1,13 +1,43 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using FacebookClone.Presentation.ConnectionMapping;
+using Microsoft.AspNetCore.SignalR;
 
 namespace FacebookClone.Presentation.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        private readonly static ConnectionMapping<string> _connections =
+       new ConnectionMapping<string>();
+        public async Task SendMessage(string sender, string receiver, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+            foreach (var connectionId in _connections.GetConnections(receiver))
+            {
+                Clients.Client(connectionId).SendAsync("ReceiveMessage", sender, message);
+            }
+        }
+        public void OnConnected(string name)
+        {
+            _connections.Add(name, Context.ConnectionId);
+        }
+        public Task OnDisconnected(bool stopCalled)
+        {
+            string name = Context.User.Identity.Name;
+
+            _connections.Remove(name, Context.ConnectionId);
+
+            return OnDisconnected(stopCalled);
         }
 
+        public Task OnReconnected()
+        {
+            string name = Context.User.Identity.Name;
+
+            if (!_connections.GetConnections(name).Contains(Context.ConnectionId))
+            {
+                _connections.Add(name, Context.ConnectionId);
+            }
+
+            return OnReconnected();
+        }
     }
 }
